06a665f1cb29d6ce4e4ee78adfecff2b96fde6ef
Annabel <annabel.gabriel@marquette.edu>
2026-09-08 17:25:00 -0500

    "Lab 1 - Pt 2"

--- AG_Lab1/main.cpp
+++ AG_Lab1/main.cpp
@@ -1,33 +1,57 @@
 /* Name: Annabel Gabriel
-   Date: 9/1/2026
-   Description: Main Function - Demonstrates Binary Utility Functions
+   Date: 9/8/2026
+   Description: Main Function - Blinks Light at Set Speeds
 */
 
 #include "mbed.h"
 #include "USBSerial.h"
 #include "AGabriel_binaryutils.hpp"
 
-#define LED1 LED_BUILTIN
-#define delay 1000 //1 HZ = 1 sec delay
-#define Register_Address (uint32_t*)0x50000504 //P0
-#define DIR (uint32_t*)0x50000514
-#define Register (uint8_t)13
+#define Register_Address (uint32_t*)0x50000504 //Out pin
+#define DIR (uint32_t*)0x50000514 //DIR (for setup)
+#define Register (uint8_t)13 //Register 13 for for P0.13
+enum Blink_Speed{NO_ERROR = 1000, //100% duty cycle
+ATTN_REQ = 500, //50% duty cycle
+FATAL_ERROR = 100}; //10% duty cycle
+
+
 
 // main() runs in its own thread in the OS
 USBSerial serial;
 
 int main(){
 
-    while(1){
+//initialize state varibles to zero/default state
+int state = 0;
+int delay = NO_ERROR;
 
-    setbit(DIR, Register);
+    while (true) {
 
-    setbit(Register_Address, Register);
-    thread_sleep_for(delay);
+        switch (state) {
+            case 0:
+                delay = NO_ERROR; //1000 ms
+                break;
 
-    clearbit(Register_Address, Register);
-    thread_sleep_for(delay);
+            case 1:
+                delay = ATTN_REQ; //500 ms
+                break;
 
+            case 2:
+                delay = FATAL_ERROR; //100 ms
+                break;
+        }
 
+        // Blink for 2 seconds in current state
+        for (int i = 0; i < 2000 / (2 * delay); i++) {
+
+            setbit(Register_Address, Register);
+            thread_sleep_for(delay);
+
+            clearbit(Register_Address, Register);
+            thread_sleep_for(delay);
+        }
+
+        // Switch state for cases above
+        state = (state + 1) % 3;
+        }
     }
-}
