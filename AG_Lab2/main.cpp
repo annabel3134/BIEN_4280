0e0deb6ab72b2ea8f9fd426b0eee6d78465e38fd
Annabel <annabel.gabriel@marquette.edu>
2026-09-24 08:03:04 -0500

    "finished pt. 3"

--- AG_Lab2/main.cpp
+++ AG_Lab2/main.cpp
@@ -1,5 +1,401 @@
+/* Name: Annabel Gabriel
+   Date: 9/23/2026
+   Description: Main Function - All 3 RGB lights glow at different rates and use queues and memory pool to do so
+*/
 
+//Part 3 of the lab
 #include "mbed.h"
+#include "USBSerial.h"
+#include "AGabriel_binaryutils.hpp"
+#include "nrf_pwm.h"
+
+//Define all registers
+#define DIR      (uint32_t*)0x50000514
+#define GPIO_OUT (uint32_t*)0x50000504
+
+// LED Declaration
+#define LED_RED_PIN   (uint8_t)24
+#define LED_GREEN_PIN (uint8_t)16
+#define LED_BLUE_PIN  (uint8_t)6
+
+
+//Initialize Threads
+Thread vanillaThread;
+Thread chocolateThread;
+Thread strawberryThread;
+Thread iceCreamManThread;
+
+
+// Struct for messages - for queue and mempool
+struct ice_cream_message_t
+{
+    float duty;
+};
+
+
+//Declare mempool
+MemoryPool<ice_cream_message_t, 32> iceCreamPool;
+
+//Declare all queues - one for each flavor
+Queue<ice_cream_message_t, 16> vanillaQueue;
+Queue<ice_cream_message_t, 16> chocolateQueue;
+Queue<ice_cream_message_t, 16> strawberryQueue;
+
+
+// Ticker used for vanilla - faster than usual bitset and such (sleep)
+Ticker vanillaTicker;
+
+float vanillaDuty = 0.0f;
+
+#define VANILLA_TICK 100
+
+
+//Function called by ticker
+void vanillaISR()
+{
+    static int counter = 0;
+
+    int Ticks = (int)(vanillaDuty * 100.0f);
+
+    // Turn LED on and off by duty cycle
+    if (counter < Ticks)
+    {
+        clearbit(GPIO_OUT, LED_GREEN_PIN);
+    }
+    else
+    {
+        setbit(GPIO_OUT, LED_GREEN_PIN);
+    }
+
+    counter++;
+
+    if (counter >= 100)
+    {
+        counter = 0;
+    }
+}
+
+
+//Vanilla thread
+void vanilla()
+{
+    //Initialize LED
+    setbit(DIR, LED_GREEN_PIN);
+    setbit(GPIO_OUT, LED_GREEN_PIN);
+
+    //Start ticker
+    vanillaTicker.attach_us(&vanillaISR, VANILLA_TICK);
+
+    while (true)
+    {
+        //Get event from queue continuously
+        osEvent evt = vanillaQueue.get();
+
+        //If new message, change duty cycle
+        if (evt.status == osEventMessage)
+        {
+            ice_cream_message_t *msg =
+                (ice_cream_message_t *)evt.value.p;
+
+            vanillaDuty = msg->duty;
+
+            //Free message after using it
+            iceCreamPool.free(msg);
+        }
+    }
+}
+
+
+//Chocolate thread
+void chocolate()
+{
+    //Initialize LED using Mbed PwmOut
+    PwmOut blue(PinName(LED_BLUE_PIN));
+
+    //Set PWM period
+    blue.period_ms(10);
+
+    while (true)
+    {
+        //Get message from queue
+        osEvent evt = chocolateQueue.get();
+
+        //If new message, change duty cycle
+        if (evt.status == osEventMessage)
+        {
+            ice_cream_message_t *msg =
+                (ice_cream_message_t *)evt.value.p;
+
+            float duty = msg->duty;
+
+            //Turn LED on for set time (active low)
+            blue.write(1.0f - duty);
+
+            //Free message after using it
+            iceCreamPool.free(msg);
+        }
+    }
+}
+
+
+//Strawberry thread
+void strawberry()
+{
+    //Build PWM sequence
+    static uint16_t strawberrySequence[202];
+
+    int i = 0;
+
+    //Fade LED in
+    for (int duty = 0; duty <= 1000; duty += 10)
+    {
+        strawberrySequence[i++] = duty;
+    }
+
+    //Fade LED out
+    for (int duty = 1000; duty >= 0; duty -= 10)
+    {
+        strawberrySequence[i++] = duty;
+    }
+
+
+    //Initialize pins being used (LED)
+    uint32_t pins[NRF_PWM_CHANNEL_COUNT] =
+    {
+        LED_RED_PIN,
+        NRF_PWM_PIN_NOT_CONNECTED,
+        NRF_PWM_PIN_NOT_CONNECTED,
+        NRF_PWM_PIN_NOT_CONNECTED
+    };
+
+    //Set pins
+    nrf_pwm_pins_set(NRF_PWM1, pins);
+
+    //Configure PWM
+    nrf_pwm_configure(
+        NRF_PWM1,
+        NRF_PWM_CLK_1MHz,
+        NRF_PWM_MODE_UP,
+        1000
+    );
+
+    //Set decoder
+    nrf_pwm_decoder_set(
+        NRF_PWM1,
+        NRF_PWM_LOAD_COMMON,
+        NRF_PWM_STEP_AUTO
+    );
+
+    //Create PWM sequence
+    nrf_pwm_sequence_t const seq =
+    {
+        .values.p_common = strawberrySequence,
+        .length          = NRF_PWM_VALUES_LENGTH(strawberrySequence),
+        .repeats         = 0,
+        .end_delay       = 0
+    };
+
+    //Set sequence
+    nrf_pwm_sequence_set(
+        NRF_PWM1,
+        0,
+        &seq
+    );
+
+    //Auto loop was not working
+    NRF_PWM1->LOOP = 0;
+
+    //Enable PWM
+    nrf_pwm_enable(NRF_PWM1);
+
+    //Clear the PWM event
+    nrf_pwm_event_clear(
+        NRF_PWM1,
+        NRF_PWM_EVENT_SEQEND0
+    );
+
+    //Start PWM sequence
+    nrf_pwm_task_trigger(
+        NRF_PWM1,
+        NRF_PWM_TASK_SEQSTART0
+    );
+
+    while (true)
+    {
+        //Restart the PWM when the sequence finishes
+        if (nrf_pwm_event_check(
+                NRF_PWM1,
+                NRF_PWM_EVENT_SEQEND0))
+        {
+            nrf_pwm_event_clear(
+                NRF_PWM1,
+                NRF_PWM_EVENT_SEQEND0
+            );
+
+            nrf_pwm_task_trigger(
+                NRF_PWM1,
+                NRF_PWM_TASK_SEQSTART0
+            );
+        }
+
+        //Check Strawberry queue for messages
+        osEvent evt = strawberryQueue.get();
+
+        //If new message, process the duty cycle
+        if (evt.status == osEventMessage)
+        {
+            ice_cream_message_t *msg =
+                (ice_cream_message_t *)evt.value.p;
+
+            //Free message after using it
+            iceCreamPool.free(msg);
+        }
+
+        thread_sleep_for(1);
+    }
+}
+
+
+//Producer - get it...he's the ice cream man!
+void ice_cream_man()
+{
+    //Duty cycles for each flavor
+    float vanillaDuty = 0.0f;
+    float chocolateDuty = 0.0f;
+    float strawberryDuty = 0.0f;
+
+    //Different step sizes create different glow rates
+    float vanillaStep = 0.01f;
+    float chocolateStep = 0.01f;
+    float strawberryStep = 0.01f;
+
+    while (true)
+    {
+        //Send vanilla message through queue
+        ice_cream_message_t *vanillaMsg =
+            iceCreamPool.alloc();
+
+        if (vanillaMsg != nullptr)
+        {
+            vanillaMsg->duty = vanillaDuty;
+
+            if (vanillaQueue.put(vanillaMsg) != osOK)
+            {
+                iceCreamPool.free(vanillaMsg);
+            }
+        }
+
+        //Change vanilla duty cycle
+        vanillaDuty += vanillaStep;
+
+        if (vanillaDuty >= 1.0f)
+        {
+            vanillaDuty = 1.0f;
+            vanillaStep = -0.01f;
+        }
+
+        if (vanillaDuty <= 0.0f)
+        {
+            vanillaDuty = 0.0f;
+            vanillaStep = 0.01f;
+        }
+
+
+        //Send chocolate message through queue
+        ice_cream_message_t *chocolateMsg =
+            iceCreamPool.alloc();
+
+        if (chocolateMsg != nullptr)
+        {
+            chocolateMsg->duty = chocolateDuty;
+
+            if (chocolateQueue.put(chocolateMsg) != osOK)
+            {
+                iceCreamPool.free(chocolateMsg);
+            }
+        }
+
+        //Change chocolate duty cycle
+        chocolateDuty += chocolateStep;
+
+        if (chocolateDuty >= 1.0f)
+        {
+            chocolateDuty = 1.0f;
+            chocolateStep = -0.01f;
+        }
+
+        if (chocolateDuty <= 0.0f)
+        {
+            chocolateDuty = 0.0f;
+            chocolateStep = 0.01f;
+        }
+
+
+        //Send strawberry message through queue
+        ice_cream_message_t *strawberryMsg =
+            iceCreamPool.alloc();
+
+        if (strawberryMsg != nullptr)
+        {
+            strawberryMsg->duty = strawberryDuty;
+
+            if (strawberryQueue.put(strawberryMsg) != osOK)
+            {
+                iceCreamPool.free(strawberryMsg);
+            }
+        }
+
+        //Change strawberry duty cycle
+        strawberryDuty += strawberryStep;
+
+        if (strawberryDuty >= 1.0f)
+        {
+            strawberryDuty = 1.0f;
+            strawberryStep = -0.01f;
+        }
+
+        if (strawberryDuty <= 0.0f)
+        {
+            strawberryDuty = 0.0f;
+            strawberryStep = 0.01f;
+        }
+
+
+        //Producer controls the update rate
+        thread_sleep_for(20);
+    }
+}
+
+
+int main()
+{
+    //Set pin direction
+    setbit(DIR, LED_GREEN_PIN);
+    setbit(DIR, LED_BLUE_PIN);
+    setbit(DIR, LED_RED_PIN);
+
+    //Turn LEDs off initially
+    setbit(GPIO_OUT, LED_GREEN_PIN);
+    setbit(GPIO_OUT, LED_BLUE_PIN);
+    setbit(GPIO_OUT, LED_RED_PIN);
+
+    //Start consumer threads
+    vanillaThread.start(vanilla);
+    chocolateThread.start(chocolate);
+    strawberryThread.start(strawberry);
+
+    //Start producer
+    iceCreamManThread.start(ice_cream_man);
+
+    while (true)
+    {
+        //Spinning empty loop
+        thread_sleep_for(1000);
+    }
+}
+
+//Part 2 of the lab
+/* #include "mbed.h"
 #include "USBSerial.h"
 #include "AGabriel_binaryutils.hpp"
 #include "nrf_pwm.h"
@@ -305,3 +701,4 @@ int main()
     }
 }
 
+*/
